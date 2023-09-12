08f7d5f4062d024cc1205e604217b629bd39ae20
David Zhu <shaowenzhu@dal.ca>
2023-09-11 21:11:02 -0300

    Mon Sep 11 21:11:02 ADT 2023

--- golang/easy.go
+++ golang/easy.go
@@ -1,1 +1,37 @@
 package golang
+
+import (
+	"strings"
+)
+
+func longestCommonPrefix(s []string) string {
+	pref := s[0]
+	for i := 1; i < len(s); i++ {
+		for !strings.HasPrefix(s[i], pref) {
+			pref = pref[:len(pref)-1]
+		}
+	}
+	return pref
+}
+
+func isValid(s string) bool {
+	if len(s) == 0 || len(s)%2 == 1 {
+		return false
+	}
+	pairs := map[rune]rune{
+		'(': ')',
+		'{': '}',
+		'[': ']',
+	}
+	stack := make([]rune, 0)
+	for _, r := range s {
+		if _, ok := pairs[r]; ok {
+			stack = append(stack, r)
+		} else if len(stack) == 0 || pairs[stack[len(stack)-1]] != r {
+			return false
+		} else {
+			stack = stack[:len(stack)-1]
+		}
+	}
+	return len(stack) == 0
+}
--- golang/esay-done-by-myself.go
+++ golang/esay-done-by-myself.go
@@ -1,1 +1,38 @@
 package golang
+
+// IsValid stack
+func IsValid(s string) bool {
+	// this could be refactored to "if len(s) == 0 || len(s)%2 == 1"
+	if len(s) == 0 {
+		return true
+	}
+	if len(s) == 1 {
+		return false
+	}
+	// this could be a map
+	var dict = [127]byte{
+		'(': ')',
+		'[': ']',
+		'{': '}',
+	}
+	stack := make([]rune, 0)
+	for _, ch := range s {
+		if ch == '{' || ch == '[' || ch == '(' {
+			stack = append(stack, ch)
+		}
+		// key point: when there is a close parentheses,
+		// the top on the stack must be the parentheses corresponding to it
+		// otherwise this cannot be closed correctly.
+		if ch == '}' || ch == ']' || ch == ')' {
+			if len(stack) == 0 {
+				return false
+			}
+			if dict[stack[len(stack)-1]] == byte(ch) {
+				stack = stack[:len(stack)-1]
+				continue
+			}
+			return false
+		}
+	}
+	return len(stack) == 0
+}
