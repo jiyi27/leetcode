e738682bc574b3c977184dfebbed500e321bcfd2
David Zhu <shaowenzhu@dal.ca>
2023-09-13 08:53:57 -0300

    Wed Sep 13 08:53:57 ADT 2023

--- golang/easy.go
+++ golang/easy.go
@@ -35,3 +35,22 @@ func isValid(s string) bool {
 	}
 	return len(stack) == 0
 }
+
+// binary search
+func searchInsert(nums []int, target int) int {
+	l := 0
+	r := len(nums) - 1
+	for l <= r {
+		m := (r + l) / 2
+		v := nums[m]
+		switch {
+		case v < target:
+			l = m + 1
+		case v > target:
+			r = m - 1
+		default:
+			return m
+		}
+	}
+	return l
+}
