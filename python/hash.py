992d4b8bcadbe1369e55d1e3b7fd0aaa27cf5c1a
David Zhu <shaowenzhu@dal.ca>
2024-02-04 10:17:50 -0400

    day14

--- python/hash.py
+++ python/hash.py
@@ -1,3 +1,31 @@
+def isHappy(n):
+    record = {n}
+    while True:
+        sum_square = 0
+        while n:
+            remainder = n % 10
+            n = n // 10
+            sum_square += remainder * remainder
+
+        if sum_square == 1:
+            return True
+        if sum_square in record:
+            return False
+        record.add(sum_square)
+        n = sum_square
+
+
+def intersection(nums1, nums2):
+    res = set()
+    record = {}
+    for num in nums1:
+        record[num] = 1
+    for num in nums2:
+        if num in record and record[num] == 1:
+            res.add(num)
+    return list(res)
+
+
 def isAnagram(s, t):
     if len(s) != len(t):
         return False
