425a5251dd31d2dac7d775f4bbe745a9654d68aa
David Zhu <shaowenzhu@dal.ca>
2024-02-05 19:23:11 -0400

    day15

--- python/hash.py
+++ python/hash.py
@@ -1,3 +1,28 @@
+def fourSumCount(nums1, nums2, nums3, nums4):
+    record = {}
+    for i in nums1:
+        for j in nums2:
+            if i + j in record:
+                record[i + j] += 1
+            else:
+                record[i + j] = 1
+    count = 0
+    for i in nums3:
+        for j in nums4:
+            if -(i + j) in record:
+                count += record[-(i + j)]
+    return count
+
+
+def twoSum(nums, target):
+    num_to_index = {}
+    for i in range(len(nums)):
+        if target - nums[i] in num_to_index:
+            return [num_to_index[target - nums[i]], i]
+        num_to_index[nums[i]] = i
+    return []
+
+
 def isHappy(n):
     record = {n}
     while True:
