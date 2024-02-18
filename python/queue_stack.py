350afb688ac545d3fcd040c51e7da0fc1fe5cbe0
David Zhu <shaowenzhu@dal.ca>
2024-02-17 20:23:10 -0400

    day 21

--- python/queue_stack.py
+++ python/queue_stack.py
@@ -1,3 +1,49 @@
+from collections import deque
+
+
+class MyStack(object):
+
+    def __init__(self):
+        self.queue_in = deque()
+        self.queue_out = deque()
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: None
+        """
+        self.queue_in.append(x)
+
+    def pop(self):
+        """
+        :rtype: int
+        """
+        if self.empty():
+            return None
+
+        for i in range(len(self.queue_in) - 1):
+            self.queue_out.append(self.queue_in.popleft())
+
+        self.queue_in, self.queue_out = self.queue_out, self.queue_in  # 交换in和out，这也是为啥in只用来存
+        return self.queue_out.popleft()
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        # 写法一：
+        if self.empty():
+            return None
+
+        return self.queue_in[-1]
+
+    def empty(self):
+        """
+        :rtype: bool
+        """
+        return len(self.queue_in) == 0
+
+
 class MyQueue(object):
 
     def __init__(self):
