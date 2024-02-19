c714c6f7ca483f0743e896414245d0ffaa033d35
David Zhu <shaowenzhu@dal.ca>
2024-02-18 23:59:07 -0400

    day 22

--- python/queue_stack.py
+++ python/queue_stack.py
@@ -1,6 +1,25 @@
 from collections import deque
 
 
+def removeDuplicatesTwoPointer(self, s):
+    res = list(s)
+    slow = fast = 0
+    length = len(res)
+
+    while fast < length:
+        # 如果一样直接换，不一样会把后面的填在slow的位置
+        res[slow] = res[fast]
+
+        # 如果发现和前一个一样，就退一格指针
+        if slow > 0 and res[slow] == res[slow - 1]:
+            slow -= 1
+        else:
+            slow += 1
+        fast += 1
+
+    return ''.join(res[0: slow])
+
+
 def removeDuplicates(s):
     res = list()
     for item in s:
