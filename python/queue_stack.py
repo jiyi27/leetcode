b915b06de49df1df16ed5a0f77cb6a4a27e35821
David Zhu <shaowenzhu@dal.ca>
2024-02-27 11:19:08 -0400

    day 25

--- python/queue_stack.py
+++ python/queue_stack.py
@@ -1,6 +1,23 @@
 from collections import deque
 
 
+def topKFrequent(nums, k):
+    count = {}
+    freq = [[] for i in range(len(nums) + 1)]
+
+    for n in nums:
+        count[n] = 1 + count.get(n, 0)
+    for n, c in count.items():
+        freq[c].append(n)
+
+    res = []
+    for i in range(len(freq) - 1, 0, -1):
+        for n in freq[i]:
+            res.append(n)
+            if len(res) == k:
+                return res
+
+
 def evalRPN(tokens):
     def resolves(a, b, Operator):
         if Operator == '+':
