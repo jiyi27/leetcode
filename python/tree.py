1295e52c28ff2a0ab2c12618ecb9791234e93722
David Zhu <shaowenzhu@dal.ca>
2024-03-04 15:20:32 -0400

    day 29

--- python/tree.py
+++ python/tree.py
@@ -1,3 +1,41 @@
+from collections import deque
+
+
+class Solution:
+    def isMirror(self, left, right):
+        if not left and not right:
+            return True
+        if not left or not right:
+            return False
+
+        return (left.val == right.val
+                and self.isMirror(left.right, right.left)
+                and self.isMirror(left.left, right.right))
+
+    def isSymmetric(self, root):
+        return self.isMirror(root.left, root.right)
+
+
+def invertTree(root):
+    if root is None:
+        return None
+
+    queue = deque()
+    queue.append(root)
+    while queue:
+        # invert
+        node_ = queue.popleft()
+        temp_node = node_.left
+        node_.left = node_.right
+        node_.right = temp_node
+
+        if node_.left:
+            queue.append(node_.left)
+        if node_.right:
+            queue.append(node_.right)
+    return root
+
+
 def postorderTraversal(root):
     def traverse(res_, node):
         if node is None:
