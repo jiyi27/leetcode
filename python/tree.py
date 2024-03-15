6d15d1d58ecb95b2a359e1eec1b8a4e992e7adff
David Zhu <shaowenzhu@dal.ca>
2024-03-15 11:13:20 -0300

    day 38

--- python/tree.py
+++ python/tree.py
@@ -1,6 +1,17 @@
 from collections import deque
 
 
+class Solution105:
+    def buildTree(self, preorder, inorder):
+        if not preorder or not inorder:
+            return None
+        root = TreeNode(preorder[0])
+        mid = inorder.index(preorder[0])
+        root.left = self.buildTree(preorder[1:mid + 1], inorder[:mid])
+        root.right = self.buildTree(preorder[mid + 1:], inorder[mid + 1:])
+        return root
+
+
 def hasPathSum112(root, targetSum):
     def travel(node, sum_):
         sum_ += node.val
