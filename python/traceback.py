865be72f9707ad6dece5d6fc9dab59f5c4bcc89a
David Zhu <shaowenzhu@dal.ca>
2024-05-05 18:58:06 -0300

    53

--- python/traceback.py
+++ python/traceback.py
@@ -1,3 +1,19 @@
+def combinationSum216(k, n):
+    path = []
+    result = []
+    def traceback(start):
+        if len(path) == k and sum(path) == n:
+            result.append(path[:])
+        for i in range(start, 10):
+            if start > n:
+                break
+            path.append(i)
+            traceback(i + 1)
+            path.pop()
+    traceback(1)
+    return result
+
+
 def combine77(n, k):
     res = []
     path = []
