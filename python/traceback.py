23f8208215a97cefbf3a9f75799cd805914e61cf
David Zhu <shaowenzhu@dal.ca>
2024-05-06 10:24:50 -0300

    54

--- python/traceback.py
+++ python/traceback.py
@@ -1,3 +1,36 @@
+def letterCombinations17(self, digits):
+        letter_combination = []
+        result = []
+        letter_map = [
+            " ",
+            " ",
+            "abc",  # 2
+            "def",  # 3
+            "ghi",  # 4
+            "jkl",  # 5
+            "mno",  # 6
+            "pqrs", # 7
+            "tuv",  # 8
+            "wxyz"  # 9
+        ]
+
+        def backtracing(current_index):
+            if current_index == len(digits):
+                result.append(''.join(letter_combination))
+                return
+
+            digit = int(digits[current_index])
+            for letter in letter_map[digit]:
+                    letter_combination.append(letter)
+                    backtracing(current_index + 1)
+                    letter_combination.pop()
+        if len(digits) == 0:
+            return result
+        backtracing(0)
+        return result
+
+
+
 def combinationSum216(k, n):
     path = []
     result = []
