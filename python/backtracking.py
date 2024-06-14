9ccdf272186f6f737a5d6fbb3310207b71a808f1
David Zhu <shaowenzhu@dal.ca>
2024-06-14 10:10:49 -0300

    Fri Jun 14 10:10:49 ADT 2024

---
+++ python/backtracking.py
@@ -0,0 +1,94 @@
+def combinationSum79(candidates, target):
+    """
+    :type candidates: List[int]
+    :type target: int
+    :rtype: List[List[int]]
+    """
+    path = []
+    result = []
+
+    def backtracking(cur_index, total):
+        if total == target:
+            result.append(path[:])
+            return
+
+        for i in range(cur_index, len(candidates)):
+            if total + candidates[i] > target:
+                break
+            total += candidates[i]
+            path.append(candidates[i])
+            backtracking(i, total)
+            total -= candidates[i]
+            path.pop()
+
+    candidates.sort()
+    backtracking(0, 0)
+    return result
+
+
+def letterCombinations17(digits):
+    letter_combination = []
+    result = []
+    letter_map = [
+        " ",
+        " ",
+        "abc",  # 2
+        "def",  # 3
+        "ghi",  # 4
+        "jkl",  # 5
+        "mno",  # 6
+        "pqrs",  # 7
+        "tuv",  # 8
+        "wxyz"  # 9
+    ]
+
+    def backtracking(current_index):
+        if current_index == len(digits):
+            result.append(''.join(letter_combination))
+            return
+
+        digit = int(digits[current_index])
+        for letter in letter_map[digit]:
+            letter_combination.append(letter)
+            backtracking(current_index + 1)
+            letter_combination.pop()
+
+    if len(digits) == 0:
+        return result
+    backtracking(0)
+    return result
+
+
+def combinationSum216(k, n):
+    path = []
+    result = []
+
+    def traceback(start):
+        if len(path) == k and sum(path) == n:
+            result.append(path[:])
+        for i in range(start, 10):
+            if start > n:
+                break
+            path.append(i)
+            traceback(i + 1)
+            path.pop()
+
+    traceback(1)
+    return result
+
+
+def combine77(n, k):
+    res = []
+    path = []
+
+    def traceback(start):
+        if len(path) == k:
+            res.append(path[:])
+            return
+        for i in range(start, n + 1):
+            path.append(i)
+            traceback(i + 1)
+            path.pop()
+
+    traceback(1)
+    return res
