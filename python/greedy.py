6f43dea083469b653dd1972e533a32829ce30654
David Zhu <shaowenzhu@dal.ca>
2024-11-01 19:50:56 -0300

    day 57

--- python/greedy.py
+++ python/greedy.py
@@ -19,21 +19,20 @@ def GiveCandy(g: [int], s: [int]) -> int:
 # 这句描述太有迷惑性了
 # 若数组含有一个数, 则返回1
 # 这题的重要的点在于, [1, 1] 应该返回 1, 而不是0, 因为可以删除一个1,从而得到一个子序列 [1]
-class Solution:
-    def wiggleMaxLength(self, nums: List[int]) -> int:
-        if len(nums) <= 1:
-            return len(nums)
+def wiggleMaxLength(nums: List[int]) -> int:
+    if len(nums) <= 1:
+        return len(nums)
 
-        preDiff = 0  # 技巧1, 使用 preDiff 记录前一个差值, 避免重复计算
-        curDiff = 0
-        amount = 1  # 核心在于此, 即只要数组有值, 则至少有一个子序列
-        for i in range(len(nums) - 1):
-            curDiff = nums[i] - nums[i + 1]
-            if (preDiff >= 0 and curDiff < 0) or (preDiff <= 0 and curDiff > 0):
-                amount += 1
-                preDiff = curDiff
+    preDiff = 0  # 技巧1, 使用 preDiff 记录前一个差值, 避免重复计算
+    curDiff = 0
+    amount = 1  # 核心在于此, 即只要数组有值, 则至少有一个子序列
+    for i in range(len(nums) - 1):
+        curDiff = nums[i] - nums[i + 1]
+        if (preDiff >= 0 and curDiff < 0) or (preDiff <= 0 and curDiff > 0):
+            amount += 1
+            preDiff = curDiff
 
-        return amount
+    return amount
 
 
 def maxSubArrayBruteForce(nums: List[int]) -> int:
@@ -83,4 +82,18 @@ def maxProfit(self, prices: List[int]) -> int:
         sum = max(sum + prices[i] - prices[i-1], sum)
     return sum
 
-    
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        if len(nums) == 1:
+            return True
+
+        cover = 0
+        i = 0
+        # python不支持动态修改for循环中变量,使用while循环代替
+        while i <= cover:
+            cover = max(i + nums[i], cover)
+            if cover >= len(nums) - 1:
+                return True
+            i += 1
+        return False
