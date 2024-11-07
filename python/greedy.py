da72e348cab0fb85184929fb7850d9a21356216a
David Zhu <shaowenzhu@dal.ca>
2024-11-07 13:14:50 -0400

    day 60

--- python/greedy.py
+++ python/greedy.py
@@ -127,13 +127,80 @@ def jump2(nums: List[int]) -> int:
     return ans
 
 
-class Solution:
-    def largestSumAfterKNegations(self, nums: List[int], k: int) -> int:
-        nums.sort(key=lambda x: abs(x), reverse=True)
-        for i in range(len(nums)):
-            if nums[i] < 0 and k > 0:
-                nums[i] = nums[i] * -1
-                k -= 1
-        if k % 2 == 1:
-            nums[-1] = nums[-1] * -1
-        return sum(nums)
+def largestSumAfterKNegations(nums: List[int], k: int) -> int:
+    nums.sort(key=lambda x: abs(x), reverse=True)
+    for i in range(len(nums)):
+        if nums[i] < 0 and k > 0:
+            nums[i] = nums[i] * -1
+            k -= 1
+    if k % 2 == 1:
+        nums[-1] = nums[-1] * -1
+    return sum(nums)
+
+
+# --------------- canCompleteCircuitBruteForce -------------------
+def canCompleteCircuitBruteForce(gas: List[int], cost: List[int]) -> int:
+    for i in range(len(cost)):
+        rest = gas[i] - cost[i]
+        index = (i + 1) % len(cost)  # 下一个加油站的索引
+
+        while rest > 0 and index != i:  # 模拟以i为起点行驶一圈（如果有rest==0，那么答案就不唯一了）
+            rest += gas[index] - cost[index]  # 更新剩余油量
+            index = (index + 1) % len(cost)  # 更新下一个加油站的索引
+
+        if rest >= 0 and index == i:  # 如果以i为起点跑一圈，剩余油量>=0，并且回到起始位置
+            return i  # 返回起始位置i
+
+    return -1  # 所有起始位置都无法环绕一圈，返回-1
+
+
+# 找出逻辑错误,
+# 看着像遍历了一圈, 其实但实际上它并没有完整地走完一圈，
+# j 每次改变后 j = j % len(gas), 重新进入 for 循环, 仍会按照之前的数字递增, 而不是求余之后的数字加一
+def canCompleteCircuitWithError(self, gas: List[int], cost: List[int]) -> int:
+    for i in range(len(gas)):
+        total_gas = 0
+        for j in range(i, i + len(gas)):
+            j = j % len(gas)
+            total_gas += gas[j] - cost[j]
+            if total_gas < 0:
+                break
+        if total_gas >= 0:
+            return i
+    return -1
+
+
+# 正确做法, 使用新的变量 idx 来作为索引, 不改变 j
+def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+    for i in range(len(gas)):
+        total_gas = 0
+        for j in range(i, i + len(gas)):
+            idx = j % len(gas)
+            total_gas += gas[idx] - cost[idx]
+            if total_gas < 0:
+                break
+        if total_gas >= 0:
+            return i
+    return -1
+
+
+# 为什么不重置 total_gas
+# total_gas 累加每一站的净油量（gas[i] - cost[i]），并且在整个行程中不重置。
+# 这是因为它用于全局判断：如果总油量 total_gas 小于 0，说明油量不足，无法完成一圈，直接返回 -1。
+# 而如果 total_gas 大于等于 0，则说明总油量足够，那么一定存在一个有效的起点，使得从该起点可以完成一圈。
+def canCompleteCircuitSolution2(gas: List[int], cost: List[int]) -> int:
+    if sum(gas) < sum(cost):
+        return -1
+    start = 0
+    total_gas = 0
+    current_gas = 0
+    for i in range(len(gas)):
+        total_gas += gas[i] - cost[i]
+        current_gas += gas[i] - cost[i]
+        # 当前累加current_gas一旦小于0，起始位置变为i+1，因为从 [start, i] 之间开始一定不行
+        if current_gas < 0:
+            start = i + 1
+            current_gas = 0
+    return start if total_gas >= 0 else -1
+
+# --------------- canCompleteCircuitBruteForce -------------------
