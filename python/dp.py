10da03d672d040db8c60ab4b21a97d27cc988ac1
David Zhu <shaowenzhu@dal.ca>
2024-11-22 12:45:49 -0400

    day 65

--- python/dp.py
+++ python/dp.py
@@ -30,24 +30,25 @@ Climbing stairs
 def climbStairs(n: int) -> int:
     if n < 2:
         return n
-    arr = [0] * (n+1)
-    arr[0] = 1
-    arr[1] = 2
+    dp = [0] * (n+1)
+    dp[0] = 1
+    dp[1] = 2
 
     for i in range(2, n):
-        arr[i] = arr[i-1] + arr[i-2]
+        dp[i] = dp[i-1] + dp[i-2]
 
-    return arr[n - 1]
+    return dp[n - 1]
 
 
 def minCostClimbingStairs(cost: List[int]) -> int:
     n = len(cost)
-    dp = [0] * (n + 1)  # dp[i]表示到达位置i的最小成本
+    dp = [0] * (n + 1)  # 到达第i台阶所花费的最少体力为dp[i]
 
     # 可以从位置0或位置1开始，所以这两个位置的成本都是0
     dp[0] = dp[1] = 0
 
-    # 到达位置i的最小成本 = 到达前面某个位置的最小成本 + 那个位置的cost值
+    # dp[i - 1] 跳到 dp[i] 需要花费 dp[i - 1] + cost[i - 1]
+    # dp[i - 2] 跳到 dp[i] 需要花费 dp[i - 2] + cost[i - 2]
     for i in range(2, n + 1):
         dp[i] = min(dp[i-1] + cost[i-1], dp[i-2] + cost[i-2])
 
