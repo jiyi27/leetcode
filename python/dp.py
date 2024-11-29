f1f384be786732b80b6893a1a2bae39e3d163cb2
David Zhu <shaowenzhu@dal.ca>
2024-11-28 23:16:57 -0400

    day 67

--- python/dp.py
+++ python/dp.py
@@ -53,3 +53,39 @@ def minCostClimbingStairs(cost: List[int]) -> int:
         dp[i] = min(dp[i-1] + cost[i-1], dp[i-2] + cost[i-2])
 
     return dp[n]
+
+
+"""
+62 Unique Path
+"""
+
+
+def uniquePathsRecursive(self, m: int, n: int) -> int:
+    def dfs(down: int, right: int) -> int:
+        # 必须检查, 不然会一直往下或者往右运动, 不会停止
+        if down > m or right > n:
+            return 0
+        if down == m and right == n:
+            return 1
+        return dfs(down+1, right) + dfs(down, right+1)
+    return dfs(1, 1)
+
+
+def uniquePaths(self, m: int, n: int) -> int:
+    # 如果 n = 3，那么 [0] * 3 会得到 [0, 0, 0]
+    # for _ in range(m) 表示这个操作要重复 m 次
+    dp = [[0] * n for _ in range(m)]
+
+    # 设置第一行和第一列的基本情况
+    for i in range(m):
+        dp[i][0] = 1
+    for j in range(n):
+        dp[0][j] = 1
+
+    # 计算每个单元格的唯一路径数
+    for i in range(1, m):
+        for j in range(1, n):
+            dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+
+    # 返回右下角单元格的唯一路径数
+    return dp[m - 1][n - 1]
